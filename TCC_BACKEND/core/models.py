c94ad1e788bd3cd635764805f7c00e433e7fe6a7
lsjv <lsjv1574@gmail.com>
2026-03-02 20:52:52 -0300

    criaçao das classes

---
+++ TCC_BACKEND/core/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+class Escola(models.Model):
+    nome = models.CharField(max_length=100)
+
+class Professor(models.Model):
+    nome = models.CharField(max_length=100)
+
+class Disciplina(models.Model):
+    nome = models.CharField(max_length=100)
+
+class Turmas(models.model):
+    nome = models.CharField(max_length=100)
+    escola = models.ForeignKey(Escola, on_delete=models.CASCADE)
+
+class Aulas(models.Model):
+    nome = models.CharField(max_length=100)
+    turmas = models.ForeignKey(Turmas, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    disciplina = models.ForeignKey(Disciplina, on_delete=models.CASCADE)
+    dias = models.IntegerField()
+    aulas = models.IntegerField()
